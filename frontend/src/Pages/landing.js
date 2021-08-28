60e2bc41116553dca24e7368e14a62a9238f362b
Olivia Misasi <misasi.olivia@gmail.com>
2021-08-28 18:59:59 -0400

    Typewriter effect for description on landing page working.

--- frontend/src/Pages/landing.js
+++ frontend/src/Pages/landing.js
@@ -3,21 +3,31 @@ import NavBar from "../Components/NavBar/LandingNavBar";
 import "./landing.scss";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Image from "../Images/landing-page-image.svg";
+import Typewriter from "typewriter-effect";
+//npm install --save typewriter-effect
 
 const Landing = () =>{
     return (
         <Container>
             <NavBar />
             <Row className = "landing">
-                <Col sm={6} className = "slogan">
+                <Col sm={5} className = "slogan">
                     <Row className="description">
-                        Overcoming imposter syndrome by tracking one accomplishWomen+ at a time.
+                    <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString('Overcoming imposter syndrome by tracking one accomplishment')
+                                .pauseFor(1500)
+                                .deleteChars(4)
+                                typewriter.typeString('Women+ at a time')
+                                .start();
+                            }}
+                            />
                     </Row>
                     <Row>
                         <Button variant="dark" className="join-now-button">JOIN NOW</Button>
                     </Row>
                 </Col>
-                <Col sm={6} >
+                <Col sm={6}>
                     <img src={Image} className="landing-image" alt="Women Allies"/>
                 </Col> 
             </Row>
