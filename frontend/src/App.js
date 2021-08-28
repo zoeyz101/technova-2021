3be0314046b4c5a85761a8225f13bef0b49688e7
Zoey Zhang <zoeyzhang2001@gmail.com>
2021-08-28 02:23:11 -0400

    adding routing and general pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Landing from "./Pages/landing";
+import Home from "./Pages/home";
+
+
+export default class App extends Component{
+
+  render(){
+    return (
+      <>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
+ 
 }
-
-export default App;
