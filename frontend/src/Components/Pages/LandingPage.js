20115bdb49894c1a446c714aea1e59699c8e2bef
Zoey Zhang <zoeyzhang2001@gmail.com>
2021-08-28 17:50:58 -0400

    making landing page more responsive

--- frontend/src/Components/Pages/LandingPage.js
+++ frontend/src/Components/Pages/LandingPage.js
@@ -1,22 +1,26 @@
 import React from "react";
-import "./styles.css";
-import Button from "react-bootstrap/Button";
+import "./styles.scss";
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import Image from "./landing-page-image.svg";
 
 const LandingPage = () => {
     return (
-        <>
-            <div>
-                <div className="description">Overcoming imposter <br></br> syndrome by tracking <br></br> one accomplishWomen+ <br></br> at a time
-                    
-                </div>
-                <Button variant="dark" className="join-now-button">JOIN NOW</Button>
+        <Container>
+            <Row className = "landing">
+                <Col sm={6} className = "slogan">
+                    <Row className="description">
+                        Overcoming imposter syndrome by tracking one accomplishWomen+ at time
+                    </Row>
+                    <Row>
+                        <Button variant="dark" className="join-now-button">JOIN NOW</Button>
+                    </Row>
+                </Col>
+                <Col sm={6} >
+                    <img src={Image} className="landing-image" alt="Landing Page Image"/>
+                </Col> 
+            </Row>
+        </Container>
 
-                <img src={Image} className="landing-image" alt="Landing Page Image"/>
-            </div>
-            
-            
-        </>
     );
   };
   
