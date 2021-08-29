c746d0e496baac15d46ac8183caef738a8f2e947
Emily Lin <44381949+emilyylin@users.noreply.github.com>
2021-08-29 02:03:12 -0400

    added username as a field in backend

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -26,6 +26,7 @@ User.findOne({ email: req.body.email }).then(user => {
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
+        username: req.body.username,
         password: req.body.password
       });
 // Hash password before saving in database
