4e380ecc9ac4865f35739463c41bf3f8dc92f0f0
Emily Lin <44381949+emilyylin@users.noreply.github.com>
2021-08-28 20:15:17 -0400

    trying to do modal

--- frontend/src/Pages/landing.js
+++ frontend/src/Pages/landing.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useState, setShow, styled} from "react";
 import NavBar from "../Components/NavBar/LandingNavBar";
 import "./landing.scss";
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal} from 'react-bootstrap';
 import Image from "../Images/landing-page-image.svg";
 
-const Landing = () =>{
+const Landing = () => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <Container>
             <NavBar />
@@ -14,14 +19,36 @@ const Landing = () =>{
                         Overcoming imposter syndrome by tracking one accomplishWomen+ at a time.
                     </Row>
                     <Row>
-                        <Button variant="dark" className="join-now-button">JOIN NOW</Button>
+                        <Button 
+                            variant="dark"
+                            oncClick={handleShow}
+                            className="join-now-button"
+                        > JOIN NOW</Button>
                     </Row>
                 </Col>
                 <Col sm={6} >
                     <img src={Image} className="landing-image" alt="Women Allies"/>
                 </Col> 
             </Row>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
+
         </Container>
+
+        
     )
 }
 
