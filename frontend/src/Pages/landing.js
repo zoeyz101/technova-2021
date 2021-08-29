6d498d15cdaf27cf4d5ac2a3441c2bb555ffc0b1
Olivia Misasi <misasi.olivia@gmail.com>
2021-08-28 21:56:07 -0400

    Login and Sign-Up modals done and fixed in front end. Need to make submit work.

--- frontend/src/Pages/landing.js
+++ frontend/src/Pages/landing.js
@@ -8,13 +8,17 @@ import Typewriter from "typewriter-effect";
 
 const Landing = () => {
 
-    const [showLogin, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const [showLogin, setLoginShow] = useState(false);
+    const [showSignUp, setSignUpShow] = useState(false);
+    const handleLoginClose = () => setLoginShow(false);
+    const handleLoginShow = () => setLoginShow(true);
+    const handleSignUpClose = () => setSignUpShow(false);
+    const handleSignUpShow = () => setSignUpShow(true);
+    
 
     return (
         <Container>
-            <NavBar isLogin = {handleShow} isSignUp = {handleShow} />
+            <NavBar isLogin = {handleLoginShow} isSignUp = {handleSignUpShow} />
             <Row className = "landing">
                 <Col sm={5} className = "slogan">
                     <Row className="description">
@@ -23,7 +27,7 @@ const Landing = () => {
                                 typewriter.typeString('Overcoming imposter syndrome by tracking one accomplishment')
                                 .pauseFor(1500)
                                 .deleteChars(4)
-                                typewriter.typeString('Women+ at a time')
+                                typewriter.typeString('Women+ at a time.')
                                 .start();
                             }}
                             />
@@ -31,7 +35,7 @@ const Landing = () => {
                     <Row>
                         <Button 
                             variant="dark"
-                            onClick={handleShow}
+                            onClick={handleSignUpShow}
                             className="join-now-button"
                         > JOIN NOW</Button>
                     </Row>
@@ -41,21 +45,46 @@ const Landing = () => {
                 </Col> 
             </Row>
 
-            <Modal centered show={showLogin} onHide={handleClose}>
-                <Modal.Body className = "signUp" >
+            <Modal centered show={showLogin} onHide={handleLoginClose}>
+                <Modal.Body className = "login" >
                     <img src={Equality} className="equality-image" alt="Everyone in Equality"/> 
                     <Form className = "form">
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Group className="mb-3" controlId="formBasicEmail" alt="Enter your email" >
+                            <Form.Control type="email" placeholder="Enter your email" />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                        <Form.Group className="mb-3" controlId="formBasicPassword" alt="Enter your password">
+                            <Form.Control type="password" placeholder="Enter your password" />
                         </Form.Group>
-                        <Button variant="primary"                             className="button" type="submit">
-                            Submit
+                        <Button variant="primary" className="button" type="submit" alt="Login Button">
+                            Log In
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+            <Modal centered show={showSignUp} onHide={handleSignUpClose}>
+                <Modal.Body className = "signup" >
+                    <img src={Equality} className="equality-image" alt="Everyone in Equality"/> 
+                    <Form className = "form">
+                        <Form.Group className="mb-3" controlId="formBasicName" alt="Enter your name">
+                            <Form.Control type="email" placeholder="Enter your name" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail" alt="Enter your email">
+                            <Form.Control type="email" placeholder="Enter your email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword" alt="Enter your password">
+                            <Form.Control type="password" placeholder="Enter your password" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formConfirmPassword" alt="Re-Enter your password">
+                            <Form.Control type="password" placeholder="Re-Enter your password" />
+                        </Form.Group>
+
+                        <Button variant="primary" className="button" type="submit" alt="Sign Up Button">
+                            Sign Up
                         </Button>
                     </Form>
                 </Modal.Body>
